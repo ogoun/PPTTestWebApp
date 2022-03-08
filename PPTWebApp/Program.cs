0196413667e4bb47dd94e7a5a737b3f17f5bea42
unknown <ogoun.er@yandex.ru>
2022-03-08 16:58:06 +0300

    Update Program.cs

--- PPTWebApp/Program.cs
+++ PPTWebApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http.Features;
 using PPTDataLibrary;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options => options.Listen(System.Net.IPAddress.Parse("0.0.0.0"), 5118));
 builder.Services.AddSingleton<PPTFilesStorage>(new PPTFilesStorage("pptfolder"));
 builder.Services.Configure<FormOptions>(x =>
 {
